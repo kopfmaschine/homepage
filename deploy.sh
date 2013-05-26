390e57d8175a533b74ee1a246a040d1e3f552d68
Jan Steinmetz <jan@kopfmaschine.com>
2013-05-26 23:51:24 +0200

    fix deployscript

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+bundle exec middleman build
 rsync -avz build kopfmaschine.com:/var/www/default/
 ssh kopfmaschine.com chgrp -R developers /var/www/default/build
 ssh kopfmaschine.com chmod -R g+w /var/www/default/build
