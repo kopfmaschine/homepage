4d67a82a41f5bec9ef3a6dd2f3dd7d322fb6a919
Jan Steinmetz <jan@kopfmaschine.com>
2013-10-17 22:52:48 +0200

    deployscript

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 bundle exec middleman build
-rsync -avz build/ kopfmaschine.com:/var/www/kopfmaschine.com/
-# ssh kopfmaschine.com chgrp -R developers /var/www/kopfmaschine.com
-# ssh kopfmaschine.com chmod -R g+w /var/www/kopfmaschine.com
+rsync -avz build/ kopfmaschine.com:/var/www/kopfmaschine.com
+ssh kopfmaschine.com chgrp -R www-data /var/www/kopfmaschine.com
+ssh kopfmaschine.com chmod -R g+w /var/www/kopfmaschine.com
