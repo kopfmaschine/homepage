d0a9887f7954716531fa5fecee6be96958c0d696
Jan Steinmetz <jan@kopfmaschine.com>
2014-08-05 14:49:02 +0200

    new address

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 bundle exec middleman build
-rsync -avz build/ kopfmaschine.com:/var/www/kopfmaschine.com
-ssh kopfmaschine.com chgrp -R www-data /var/www/kopfmaschine.com
-ssh kopfmaschine.com chmod -R g+w /var/www/kopfmaschine.com
+rsync -avz build/ -e "ssh -p 5022" kopfmaschine.com:/var/www/kopfmaschine.com
+ssh kopfmaschine.com -p 5022 chgrp -R www-data /var/www/kopfmaschine.com
+ssh kopfmaschine.com -p 5022 chmod -R g+w /var/www/kopfmaschine.com
